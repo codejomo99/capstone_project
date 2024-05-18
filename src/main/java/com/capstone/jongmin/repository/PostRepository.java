37e776b11dc5a8274f45bd55f3f1b9e5d5070ff1
Jong-Min <codejomo99@gmail.com>
2024-05-19 00:30:59 +0900

    feat : post pageable 기능

--- src/main/java/com/capstone/jongmin/repository/PostRepository.java
+++ src/main/java/com/capstone/jongmin/repository/PostRepository.java
@@ -1,8 +1,12 @@
 package com.capstone.jongmin.repository;
 
 import com.capstone.jongmin.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+  Page<Post> findByBoardId(long boardId, Pageable pageable);
+  Page<Post> findByBoardIdAndTitleContaining(long boardId, String keyword, Pageable pageable);
 
 }
--- src/main/java/com/capstone/jongmin/service/PostService.java
+++ src/main/java/com/capstone/jongmin/service/PostService.java
@@ -2,20 +2,28 @@ package com.capstone.jongmin.service;
 
 
 import com.capstone.jongmin.dto.PostRequest;
+import com.capstone.jongmin.entity.Board;
 import com.capstone.jongmin.entity.Post;
+import com.capstone.jongmin.repository.BoardRepository;
 import com.capstone.jongmin.repository.PostRepository;
+import jakarta.transaction.Transactional;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
   private final PostRepository postRepository;
+  private final BoardRepository boardRepository;
 
   // CREATE
-  public Post savePost(Post post){
-    return postRepository.save(post);
+  public Post savePost(Long boardId, Post post){
+    Board board = boardRepository.findById(boardId).orElseThrow(()-> new IllegalArgumentException("not found :" + boardId));
+    Post savePost = new Post(post.getTitle(),post.getContent(),board);
+    return postRepository.save(savePost);
   }
 
   // READ
@@ -28,6 +36,7 @@ public class PostService {
   }
 
   // UPDATE
+  @Transactional
   public Post updatePost(Post post,Long id) {
     Post findPost = postRepository.findById(id).orElseThrow(()-> new IllegalArgumentException(("not found: ")+id));
     findPost.update(post.getTitle(),post.getContent());
@@ -37,5 +46,13 @@ public class PostService {
   // DELETE
   public void deletePost(Long id) {
     postRepository.deleteById(id);
+  }
+
+  public Page<Post> getPostByBoardId(Long boardId, Pageable pageable) {
+    return postRepository.findByBoardId(boardId, pageable);
+  }
+
+  public Page<Post> searchPosts(Long boardId, String keyword, Pageable pageable) {
+    return postRepository.findByBoardIdAndTitleContaining(boardId, keyword, pageable);
   }
 }
