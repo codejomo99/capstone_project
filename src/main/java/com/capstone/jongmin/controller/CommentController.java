945bff129ae16c16329bdd2a0a5f849bf978edff
Jong-Min <codejomo99@gmail.com>
2024-05-19 01:52:07 +0900

    feat : Comment Delete, Update

--- src/main/java/com/capstone/jongmin/controller/CommentController.java
+++ src/main/java/com/capstone/jongmin/controller/CommentController.java
@@ -1,5 +1,34 @@
 package com.capstone.jongmin.controller;
 
-public class CommentController {
 
+import com.capstone.jongmin.dto.CommentRequest;
+import com.capstone.jongmin.dto.CommentResponse;
+import com.capstone.jongmin.entity.Comment;
+import com.capstone.jongmin.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CommentController {
+  private final CommentService commentService;
+
+  @DeleteMapping("/comments/{id}")
+  public ResponseEntity<Void> deleteComment(@PathVariable("id") Long id){
+    commentService.deleteComment(id);
+    return ResponseEntity.noContent().build();
+  }
+
+  @PostMapping("/comments/{id}/edit")
+  public ResponseEntity<CommentResponse> updateComment(@PathVariable("id") Long id, @RequestBody CommentRequest request){
+    Comment update = commentService.updateComment(id, request.toEntity());
+    CommentResponse commentResponse = new CommentResponse(update);
+    return ResponseEntity.ok(commentResponse);
+  }
 }
